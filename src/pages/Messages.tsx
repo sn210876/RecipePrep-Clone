db7d073b3ddf9375c0b0b1f4360c893f52ac038b
sn210876 <sn210876@gmail.com>
2025-11-17 00:29:35 -0700

    Updated Messages.tsx

--- src/pages/Messages.tsx
+++ src/pages/Messages.tsx
@@ -361,7 +361,7 @@ export function Messages({ recipientUserId, recipientUsername, onBack }: Message
 </div>
 
      {/* CENTERED FULL-WIDTH INPUT BAR */}
-<div className="fixed bottom-20 left-20 right-20 bg-white border-t border-b border-gray-200 px-4 py-3 z-50">
+<div className="fixed bottom-20 left-20 right-20 bg-none border-t border-b border-gray-200 px-4 py-3 z-50">
 
 
 
