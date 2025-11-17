4c2ba39e066b8a43822e7cfe87117ac55188b303
sn210876 <sn210876@gmail.com>
2025-11-16 17:39:47 -0700

    Updated Discover.tsx

--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -1052,7 +1052,7 @@ export function Discover({ onNavigateToMessages, onNavigate: _onNavigate, shared
                       >
                         <MessageCircle className="w-7 h-7 text-gray-700" />
                         {post._count?.comments && post._count.comments > 0 && (
-                          <span className="absolute -top-1 -right-1 bg-blue-600 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
+                          <span className="absolute -top-1 -right-1 bg-grey-600 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
                             {post._count.comments}
                           </span>
                         )}
