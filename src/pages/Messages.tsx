10254f0b33daa6a2742c10ba951b032dbcd1275d
sn210876 <sn210876@gmail.com>
2025-11-17 00:25:53 -0700

    Updated Messages.tsx

--- src/pages/Messages.tsx
+++ src/pages/Messages.tsx
@@ -372,8 +372,7 @@ export function Messages({ recipientUserId, recipientUsername, onBack }: Message
       onChange={(e) => setNewMessage(e.target.value)}
       onKeyPress={handleKeyPress}
       placeholder="Type a message..."
-      lassName="flex gap-2 w-[80%] mx-auto"
-
+      className="w-[80%] px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-orange-500"
 
     />
     <Button
