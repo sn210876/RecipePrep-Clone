170dfbd83f7d3fae896cb97ba41333653d320181
sn210876 <sn210876@gmail.com>
2025-11-17 09:48:11 -0700

    Updated Messages.tsx

--- src/pages/Messages.tsx
+++ src/pages/Messages.tsx
@@ -341,6 +341,7 @@ export function Messages({ recipientUserId, recipientUsername, onBack }: Message
   key={message.id}
   className="flex justify-end"
 >
+        ;'
         <div
           className={`max-w-[70%] rounded-2xl px-4 py-2 ${
             message.sender_id === currentUserId
