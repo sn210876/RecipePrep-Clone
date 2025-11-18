f99affb429e88cbe4cc76325f0986a1f653f6de3
sn210876 <sn210876@gmail.com>
2025-11-18 14:36:05 -0700

    Added capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mealscrape.app',
+  appName: 'MealScrape',
+  webDir: 'dist'
+};
+
+export default config;
