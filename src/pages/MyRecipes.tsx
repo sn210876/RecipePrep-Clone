536d7661dd8aaaa543679f643656566762f281b3
sn210876 <sn210876@gmail.com>
2025-11-19 17:49:38 -0700

    Updated MyRecipes.tsx

--- src/pages/MyRecipes.tsx
+++ src/pages/MyRecipes.tsx
@@ -51,22 +51,22 @@ export function MyRecipes() {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {state.savedRecipes.map((recipe) => (
-            <RecipeCard
-              key={recipe.id}
-              recipe={recipe}
-              showReviewButton={true}
-              onCook={(recipeId) => {
-                const recipe = state.savedRecipes.find(r => r.id === recipeId);
-                if (recipe) {
-                  console.log('Cook clicked for:', recipe.title);
-                  setCookingRecipe(recipe);
-                }
-              }}
-            />
-          ))}
-        </div>
+       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 min-h-0 [&>*]:flex">
+  {state.savedRecipes.map((recipe) => (
+    <RecipeCard
+      key={recipe.id}
+      recipe={recipe}
+      showReviewButton={true}
+      onCook={(recipeId) => {
+        const recipe = state.savedRecipes.find(r => r.id === recipeId);
+        if (recipe) {
+          console.log('Cook clicked for:', recipe.title);
+          setCookingRecipe(recipe);
+        }
+      }}
+    />
+  ))}
+</div>
       </div>
       {cookingRecipe && (
         <CookMode
