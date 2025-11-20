3fda3c8e4666f6176431652fb2d06b88d727686a
sn210876 <sn210876@gmail.com>
2025-11-20 06:27:31 -0700

    Updated DiscoverRecipes.tsx

--- src/pages/DiscoverRecipes.tsx
+++ src/pages/DiscoverRecipes.tsx
@@ -192,7 +192,7 @@ export function Discover({ onNavigate: _onNavigate }: DiscoverProps) {
           </h1>
           <p className="text-xl text-gray-600 max-w-2xl mx-auto">
 These recipes have been selected from all over <span className="font-bold text-blue-600">Social Media</span> & the <span className="font-bold text-blue-600"> Web</span>
-</p>
+</p>  </h1> so enjoy
         </div>
 
         <div className="mb-12 space-y-6">
