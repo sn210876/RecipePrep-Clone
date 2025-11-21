01dd858abd5a76dcdbc949965b0a2bc6d3f9362c
sn210876 <sn210876@gmail.com>
2025-11-21 11:01:38 -0700

    Updated DiscoverRecipes.tsx

--- src/pages/DiscoverRecipes.tsx
+++ src/pages/DiscoverRecipes.tsx
@@ -204,7 +204,8 @@ export function Discover({ onNavigate: _onNavigate }: DiscoverProps) {
           </p>
         </div>
       </div>
-      <div className={`grid gap-4 md:gap-6 ${isMobile ? 'grid-cols-2' : 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'}`}>
+      <div className={`grid gap-4 md:gap-6 auto-rows-fr ${isMobile ? 'grid-cols-2' : 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'}`}>
+
         {recipes.map((recipe: Recipe) => (
           <RecipeCard
             key={recipe.id}
