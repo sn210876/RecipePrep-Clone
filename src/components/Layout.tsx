32a5480bb691bd8293f2e63c60f9b1e526bf1621
sn210876 <sn210876@gmail.com>
2025-11-21 17:37:12 -0700

    Updated Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -181,7 +181,7 @@ const loadUnreadCount = async (userId: string) => {
       )}
 
       {/* Sidebar - Mobile Optimized */}
-      <aside className={`fixed left-0 top-0 z-9999 h-screen w-64 sm:w-72 transform bg-white shadow-2xl transition-transform duration-300 ease-in-out lg:translate-x-0 ${
+      <aside className={`fixed left-0 top-0 z-40 h-screen w-64 sm:w-72 transform bg-white shadow-2xl transition-transform duration-300 ease-in-out lg:translate-x-0 ${
         isMobileMenuOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'
       }`}>
         <div className="flex h-full flex-col">
