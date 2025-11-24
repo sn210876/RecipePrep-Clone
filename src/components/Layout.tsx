00542280da3b9e58fbe9672388617d3ef09a70ea
sn210876 <sn210876@gmail.com>
2025-11-23 20:15:13 -0700

    Updated Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -363,7 +363,8 @@ const loadUnreadCount = async (userId: string) => {
         </main>
 
         {/* Bottom Navigation - Hidden on Add Recipe page */}
-{currentPage !== 'add-recipe' && (  <div className="fixed bottom-0 left-0 right-0 z-40 lg:hidden bg-white border-t border-gray-200 shadow-lg safe-area-bottom">
+<div className="fixed bottom-0 left-0 right-0 z-50 lg:hidden bg-white border-t border-gray-200 shadow-lg safe-area-bottom">
+
     <div className="max-w-lg mx-auto px-4 py-3">
       <div className="flex items-center justify-around">
         
