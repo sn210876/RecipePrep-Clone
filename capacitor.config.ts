a4f7ee345940263e6862f25c76cfe58e6f43f5db
sn210876 <sn210876@gmail.com>
2025-11-24 19:57:45 -0700

    Updated capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,29 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.mealscrape.app',
   appName: 'MealScrape',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https',
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: "#FF6B35",
+      androidSplashResourceName: "splash",
+      iosSplashResourceName: "Default",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#ffffff",
+    },
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"]
+    },
+    StatusBar: {
+      style: "LIGHT",
+      backgroundColor: "#FF6B35"
+    }
+  }
 };
 
 export default config;
