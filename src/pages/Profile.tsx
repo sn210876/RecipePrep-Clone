df7c26c95d0e12944d277001b14c9f3771713a95
sn210876 <sn210876@gmail.com>
2025-11-24 18:57:31 -0700

    Updated Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1128,6 +1128,6 @@ if (loading) {
         userId={targetUserId || ''}
         type={followersModalType}
       />
-    </div>  /* ← This is the final closing div of the entire Profile component */}
+    </div> 
   );
 }
