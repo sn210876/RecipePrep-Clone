410be3b55c21c41d09ccc5fafd45e88edb4444e7
sn210876 <sn210876@gmail.com>
2025-11-25 08:52:58 -0700

    Updated Discover.tsx

--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -1730,7 +1730,7 @@ if (post.video_url) {
     }
             
             // Parse videos
-         if (post.video_url) {
+            if (post.video_url) {
       try {
         const parsed = JSON.parse(post.video_url);
         if (Array.isArray(parsed)) {
@@ -1742,8 +1742,7 @@ if (post.video_url) {
         currentMedia.push({ url: post.video_url, type: 'video' });
       }
     }
-    
-    setEditingPost({
+     setEditingPost({
       id: post.id,
       caption: post.caption || '',
       recipeUrl: post.recipe_url || '',
@@ -1754,9 +1753,7 @@ if (post.video_url) {
       newMediaTypes: []
     });
   }
-}}
-            
-            // Track deleted media
+}}       
             const deletedMedia = (editingPost as any)?.deletedMedia || [];
             const remainingMedia = mediaUrls
               .map((url, idx) => ({ url, type: mediaTypes[idx] }))
