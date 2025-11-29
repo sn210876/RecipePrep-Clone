c0262fc2627e4da3a09e210f143fa29976223e8b
sn210876 <sn210876@gmail.com>
2025-11-28 22:47:35 -0700

    Updated AddRecipe.tsx

--- src/pages/AddRecipe.tsx
+++ src/pages/AddRecipe.tsx
@@ -803,7 +803,7 @@ return (
                     <SelectTrigger className={`mt-1.5 ${errors.cuisineType ? 'border-red-500' : ''}`}>
                       <SelectValue placeholder="Select cuisine" />
                     </SelectTrigger>
-                    <SelectContent className="z-[70]">
+                    <SelectContent className="z-[300]">
                       {CUISINE_TYPES.map(type => (
                         <SelectItem key={type} value={type}>{type}</SelectItem>
                       ))}
