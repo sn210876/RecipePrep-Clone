7b1ffc88a50d320b59013872361e7aaf3b94d521
sn210876 <sn210876@gmail.com>
2025-11-28 22:34:58 -0700

    Updated GroceryList.tsx

--- src/pages/GroceryList.tsx
+++ src/pages/GroceryList.tsx
@@ -574,7 +574,7 @@ export function GroceryList({ onNavigate }: GroceryListProps = {}) {
                   </Button>
                   <Button
                     onClick={handleSendToCart}
-                    className="col-span-2 h-10 text-sm bg-gradient-to-r from-orange-500 to-red-500 hover:from-orange-600 hover:to-red-600"
+                    className="h-10 text-sm bg-gradient-to-r from-orange-500 to-red-500 hover:from-orange-600 hover:to-red-600"
                   >
                     <ShoppingCart className="w-4 h-4 mr-1" />
                     Send to Cart
