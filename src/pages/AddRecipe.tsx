feda0f851de620a473529cc5a5f8088bd34b5a95
sn210876 <sn210876@gmail.com>
2025-12-01 17:06:16 -0700

    Updated AddRecipe.tsx

--- src/pages/AddRecipe.tsx
+++ src/pages/AddRecipe.tsx
@@ -111,6 +111,7 @@ export function AddRecipe({ onNavigate }: AddRecipeProps = {}) {
     if (editId) {
       setEditRecipeId(editId);
       setIsEditMode(true);
+      setActiveTab('manual');
       loadRecipeForEdit(editId);
     }
   }, []);
