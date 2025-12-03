93a2ae6ea81fdc278748d594bb978fabb6642713
sn210876 <sn210876@gmail.com>
2025-12-02 21:23:56 -0700

    Updated FAQ.tsx

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -178,7 +178,7 @@ export function FAQ() {
                         </div>
                       </div>
                     </div>
-                  </AccordionContent>
+                  </div>
                 </AccordionItem>
         {/* Adding Recipes */}
           <Card>
