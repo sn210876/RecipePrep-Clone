188fa0610cd5ca68dd0795e7f1ab9e898c574cc1
sn210876 <sn210876@gmail.com>
2025-12-04 23:38:43 -0700

    Updated CartEnhanced.tsx

--- src/pages/CartEnhanced.tsx
+++ src/pages/CartEnhanced.tsx
@@ -541,8 +541,7 @@ export function Cart({ onNavigate }: CartProps = {}) {
     Send To Amazon Cart
   </Button>
   <p className="text-xs text-center text-gray-500 mt-2">
-    Products with ASINs are added to your Amazon cart instantly
-  </p>
+   </p>
 </div>
               </div>
             </div>
