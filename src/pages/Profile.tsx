a72cd6ac71a4759e6c2162f5ce01d80111080155
sn210876 <sn210876@gmail.com>
2025-12-07 08:51:27 -0700

    Updated Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -8,6 +8,8 @@ import { Input } from '../components/ui/input';
 import { Textarea } from '../components/ui/textarea';
 import { Button } from '../components/ui/button';
 import { Label } from '../components/ui/label';
+import { ChevronLeft } from 'lucide-react';
+
 
 import CommentModal from '../components/CommentModal';
 import { FollowersModal } from '../components/FollowersModal';
