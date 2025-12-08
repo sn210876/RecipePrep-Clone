1c4f9321b37fb3fe487646415f019b908e6af769
sn210876 <sn210876@gmail.com>
2025-12-07 23:14:04 -0700

    Updated FAQ.tsx

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -127,7 +127,7 @@ export function FAQ() {
                         <div className="mt-4">
                           <TutorialVideo
                             src="https://vohvdarghgqskzqjclux.supabase.co/storage/v1/object/public/posts/videos/Adding%20From%20Notes.mp4"
-                              width="70%"
+                              width="50%"
 
                             className="max-w-md mx-auto"
                           />
