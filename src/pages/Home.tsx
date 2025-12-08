5a3058eabc5803ae6e394c7fbdd2a6ac3bc49e6e
sn210876 <sn210876@gmail.com>
2025-12-07 23:14:59 -0700

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -24,7 +24,7 @@ export function Home({ onNavigate }: HomeProps) {
           <div className="max-w-3xl mx-auto">
             <TutorialVideo
               src="https://vohvdarghgqskzqjclux.supabase.co/storage/v1/object/public/posts/videos/How%20To%20Use%20Mealscrape.mp4"
-                width="50%"
+                width="30%"
 
               title="Welcome to Meal Scrape - Quick Start Guide"
             />
