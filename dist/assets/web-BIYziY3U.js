35ecb2511a3b9bc4b057a625786c99719ac4ebf7
sn210876 <sn210876@gmail.com>
2025-12-28 23:22:40 -0700

    Publish application

---
+++ dist/assets/web-BIYziY3U.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-CkUg_WPP.js";import"./ui-vendor-f93QKSUu.js";import"./react-vendor-Bh3ye5P1.js";import"./supabase-vendor-BRTiG-8S.js";class p extends n{async show(e){}async hide(e){}}export{p as SplashScreenWeb};
