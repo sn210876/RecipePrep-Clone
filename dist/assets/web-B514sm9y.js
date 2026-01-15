f5bef17409593c0d08cc186724e1229ad6a6ea51
sn210876 <sn210876@gmail.com>
2026-01-15 01:03:26 -0700

    Publish application

---
+++ dist/assets/web-B514sm9y.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-CW8L4bB6.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
--- dist/assets/web-BN6wujar.js
+++
@@ -1,1 +0,0 @@
-import{W as e}from"./index-DVwpH2fI.js";class r extends e{constructor(){super()}async checkSendIntentReceived(){return{title:""}}finish(){}}export{r as SendIntentWeb};
