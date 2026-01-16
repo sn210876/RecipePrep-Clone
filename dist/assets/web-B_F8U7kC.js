e9356c34f47b278f8cad68d20be822842acaa9c1
sn210876 <sn210876@gmail.com>
2026-01-16 10:27:34 -0700

    Publish application

---
+++ dist/assets/web-B_F8U7kC.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-Cf-ALqSY.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
---
+++ dist/assets/web-BiJWz1dk.js
@@ -0,0 +1,1 @@
+import{W as e}from"./index-Cf-ALqSY.js";class r extends e{constructor(){super()}async checkSendIntentReceived(){return{title:""}}finish(){}}export{r as SendIntentWeb};
