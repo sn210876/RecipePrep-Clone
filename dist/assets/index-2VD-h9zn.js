28d84be243d3cc42dd53560655cbc0bcaf073c48
sn210876 <sn210876@gmail.com>
2026-01-17 10:12:03 -0700

    Publish application

---
+++ dist/assets/index-2VD-h9zn.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DgGYZqrx.js","assets/index-RGevgwjF.js","assets/index-BSBpZIjA.css"])))=>i.map(i=>d[i]);
+import{p as e,_ as o}from"./index-RGevgwjF.js";const _=e("Browser",{web:()=>o(()=>import("./web-DgGYZqrx.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{_ as Browser};
---
+++ dist/assets/web-BGoC4AgW.js
@@ -0,0 +1,1 @@
+import{W as e}from"./index-RGevgwjF.js";class r extends e{constructor(){super()}async checkSendIntentReceived(){return{title:""}}finish(){}}export{r as SendIntentWeb};
